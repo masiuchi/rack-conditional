60ac1d6f0392e5784bf76c1de36e53653e11423b
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-09-04 20:57:23 +0900

    Implement use_if for Rails.

--- lib/rack_conditional.rb
+++ lib/rack_conditional.rb
@@ -1,2 +1,4 @@
 require 'rack/builder/conditional'
 require 'sinatra/base/conditional' if defined? Sinatra::Base
+require 'rails/configuration/conditional' \
+  if defined? Rails::Configuration::MiddlewareStackProxy
