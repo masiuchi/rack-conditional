47a2d9395ee8374b0907e41125f1a94a8abffdd2
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-09-04 21:20:44 +0900

    Fix failed test.

--- test/spec_rails.rb
+++ test/spec_rails.rb
@@ -4,7 +4,6 @@ require 'rails/configuration/conditional'
 
 describe Rails::Configuration::MiddlewareStackProxy do
   it 'has "use_if" method' do
-    Rails::Configuration::MiddlewareStackProxy.method_defined?(:use_if)
-                                              .must_equal true
+    Rails::Configuration::MiddlewareStackProxy.method_defined?(:use_if).must_equal true
   end
 end
