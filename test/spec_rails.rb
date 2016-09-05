abeb1b98b3a3d5d289b866a74c75345bb78b7292
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-09-06 00:54:13 +0900

    Skip test it the class is not defined.

--- test/spec_rails.rb
+++ test/spec_rails.rb
@@ -2,8 +2,10 @@ require 'minitest/autorun'
 require 'rails/configuration'
 require 'rails/configuration/conditional'
 
-describe Rails::Configuration::MiddlewareStackProxy do
-  it 'has "use_if" method' do
-    Rails::Configuration::MiddlewareStackProxy.method_defined?(:use_if).must_equal true
+if defined? Rails::Configuration::MiddlewareStackProxy
+  describe Rails::Configuration::MiddlewareStackProxy do
+    it 'has "use_if" method' do
+      Rails::Configuration::MiddlewareStackProxy.method_defined?(:use_if).must_equal true
+    end
   end
 end
