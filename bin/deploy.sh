5ea3f8b2aa431e94d8d60fd11a40a0460a33f3d8
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-10-01 15:11:54 +0900

    Move deploy code to deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+version_file=lib/rack/conditional/version.rb
+rubygems_username=masiuchi
+github_username="Masahiro Iuchi"
+github_email=masahiro.iuchi+circleci@gmail.com
+
+if [ -z "$CIRCLECI" ]; then
+  echo "This script runs only on CircleCI"
+  exit
+fi
+
+if [ -z "echo `git diff --name-only HEAD~` | grep $version_file" ]; then
+  echo "This commit does not change $version_file"
+  exit
+fi
+
+curl -u $rubygems_username:$RUBYGEMS_PASSWORD https://rubygems.org/api/v1/api_key.yaml > ~/.gem/credentials; chmod 0600 ~/.gem/credentials
+git config user.name $github_username
+git config user.email $github_email
+bundle exec rake build
+bundle exec rake release
