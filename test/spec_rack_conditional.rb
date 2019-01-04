89603daade84a3100330dfc6afa8b2dfa058b489
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2019-01-04 22:30:15 +0900

    Rename TestMiddleware to fix failed test on CircleCI

--- test/spec_rack_conditional.rb
+++ test/spec_rack_conditional.rb
@@ -4,30 +4,38 @@ require 'rack/lint'
 require 'rack/mock'
 
 # For test.
-class TestMiddleware
-  attr_reader :called
+module Rack
+  class Conditional
+    class TestMiddleware
+      attr_reader :called
 
-  def initialize(app)
-    @app = app
-  end
+      def initialize(app)
+        @app = app
+      end
 
-  def call(env)
-    @called = true
-    @app.call(env)
+      def call(env)
+        @called = true
+        @app.call(env)
+      end
+    end
   end
 end
 
 describe Rack::Conditional do
   it 'executes middleware' do
     condition = proc { |env| env['X_TEST_HEADER'] == 'true' }
-    conditional = Rack::Conditional.new(app, condition, TestMiddleware)
+    conditional = Rack::Conditional.new(
+      app, condition, Rack::Conditional::TestMiddleware
+    )
     Rack::Lint.new(conditional).call(request)
     conditional.instance_variable_get('@middleware').called.must_equal true
   end
 
   it 'does not execute middleware' do
     condition = proc { |env| env['X_TEST_HEADER'] == 'false' }
-    conditional = Rack::Conditional.new(app, condition, TestMiddleware)
+    conditional = Rack::Conditional.new(
+      app, condition, Rack::Conditional::TestMiddleware
+    )
     Rack::Lint.new(conditional).call(request)
     conditional.instance_variable_get('@middleware').called.must_be_nil
   end
